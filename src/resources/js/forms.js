0ada18e91d2d75cd509fd902f4625823e250e4d6
Kuroyuki-Kaze <teng.thaisothyrak@gmail.com>
2022-04-23 12:44:59 +0700

    add template.html and a form template

---
+++ src/resources/js/forms.js
@@ -0,0 +1,8 @@
+const responseSubmit1 = () => {
+    const firstName = document.getElementById('first-name1');
+    const lastName = document.getElementById('last-name1');
+    const email = document.getElementById('email1');
+    const phone = document.getElementById('phone1');
+
+    alert(`First Name = ${firstName.value}\nLast Name = ${lastName.value}\nEmail = ${email.value}\nPhone = ${phone.value}`);
+}
